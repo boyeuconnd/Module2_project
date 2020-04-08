488d174eca5c742c98fbbb752bb0868042afe9f2
Dang Pham <thienvu453@gmail.com>
2020-04-08 20:54:30 +0700

    Edit project SpellNum

--- SpellNum/src/SpellNum.java
+++ SpellNum/src/SpellNum.java
@@ -72,78 +72,17 @@ public class SpellNum {
                     spell_num = "nineteen";
                     break;
             }
-        }else if(num<100){
-            int unit = num%10;
-            int tens = num/10;
-            switch (tens){
-                case 2:
-                    spell_num = "twenty";
-                    break;
-                case 3:
-                    spell_num = "thirty";
-                    break;
-                case 4:
-                    spell_num = "fourty";
-                    break;
-                case 5:
-                    spell_num = "fifty";
-                    break;
-                case 6:
-                    spell_num = "sixty";
-                    break;
-                case 7:
-                    spell_num = "seventy";
-                    break;
-                case 8:
-                    spell_num = "eighty";
-                    break;
-                case 9:
-                    spell_num = "ninety";
-                    break;
-            }
-            switch (unit){
-                case 0:
-                    spell_num += "";
-                    break;
-                case 1:
-                    spell_num += " one";
-                    break;
-                case 2:
-                    spell_num += " two";
-                    break;
-                case 3:
-                    spell_num += " three";
-                    break;
-                case 4:
-                    spell_num += " four";
-                    break;
-                case 5:
-                    spell_num += " five";
-                    break;
-                case 6:
-                    spell_num += " six";
-                    break;
-                case 7:
-                    spell_num += " seven";
-                    break;
-                case 8:
-                    spell_num += " eight";
-                    break;
-                case 9:
-                    spell_num += " nine";
-                    break;
-                case 10:
-                    spell_num += " ten";
-                    break;
-            }
-
-        }else if(num<1000) {
+        }else if(num>=20 && num<1000) {
             int unit = num%10;
             int tens = (num/10)%10;
             int hunds = num/100;
             switch (hunds){
+                case 0:
+                    spell_num="";
+                    break;
                 case 1:
                     spell_num = "one hundred";
+                    break;
                 case 2:
                     spell_num = "two hundreds";
                     break;
