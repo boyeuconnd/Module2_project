6736f491fc66e4c4d01b577742291dd70f6a9521
Dang Pham <thienvu453@gmail.com>
2020-04-12 21:28:30 +0700

    Add project StopWatch

---
+++ OOP_project/StopWatch/src/StopWatch.java
@@ -0,0 +1,19 @@
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+    public long getStartTime(){
+        return startTime;
+    }
+    public long getEndTime(){
+        return endTime;
+    }
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void end(){
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return this.endTime-this.startTime;
+    }
+}
