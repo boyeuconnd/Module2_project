b00175e6a00eca1c541dc1a49dd6ac86bde40c61
Dang Pham <thienvu453@gmail.com>
2020-04-12 20:15:51 +0700

    Edit project PtringBacHai

--- OOP_project/PtrinhBacHai/src/QuadraticEquation.java
+++ OOP_project/PtrinhBacHai/src/QuadraticEquation.java
@@ -18,10 +18,10 @@ public class QuadraticEquation {
         return Math.pow(b,2) - 4*a*c;
     }
     public double getRoot1(){
-        return (-b + getDiscriminant())/2*a;
+        return (-b + Math.sqrt(getDiscriminant()))/2*a;
     }
     public double getRoot2(){
-        return (-b - getDiscriminant())/2*a;
+        return (-b - Math.sqrt(getDiscriminant()))/2*a;
     }
 
 }
