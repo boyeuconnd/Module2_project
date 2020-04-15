2de8a41b8947f3b2fb2df812dbb196da03e5d6e6
Dang Pham <thienvu453@gmail.com>
2020-04-15 10:24:35 +0700

    Add project Animal_Interface

---
+++ OOP_project/Animal_Interface/src/animal/Chicken.java
@@ -0,0 +1,16 @@
+package animal;
+
+import edible.Edible;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makeSound() {
+        return "Chicken: Cluck...cluck!";
+    }
+
+
+    @Override
+    public String howToEat() {
+        return "Chicken could be fried";
+    }
+}
