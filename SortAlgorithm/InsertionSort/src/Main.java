53acac31623baebda132a4c8f263826e85b77452
Dang Pham <thienvu453@gmail.com>
2020-04-27 09:55:57 +0700

    Add project InsertionSort

---
+++ SortAlgorithm/InsertionSort/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    /*Function to sort array using insertion sort*/
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+            System.out.println("Loop to element number "+i+ " of array");
+            printArray(arr);
+        }
+    }
+
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6,99,30,-3,78,1 };
+
+        Main ob = new Main();
+        System.out.println("Before sort array:");
+        printArray(arr);
+        ob.sort(arr);
+        System.out.println("Final sorted array");
+
+        printArray(arr);
+    }
+}
