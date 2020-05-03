d645f26634fc2b8f0b20c7866a576376391d2d81
Dang Pham <thienvu453@gmail.com>
2020-05-03 15:37:34 +0700

    add project RegexEmail

---
+++ String_Regex/ValidateEmail/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    private static RegexEmail emailExample;
+    public static final String[] validEmail = new String[] { "a12@gmail.com", "abcdef@yahoo.com", "abcdefgh@hotmail.com" };
+    public static final String[] invalidEmail = new String[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
+
+    public static void main(String args[]) {
+        emailExample = new RegexEmail();
+        for (String email : validEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+    }
+}
